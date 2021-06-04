e7dc7472a3a0a2abb8a5add6f8a07d04d1022a8b
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2021-06-04 01:18:56 -0600

    Haciendo Home estático como guía de diseño

--- marketplace-multivendor-cliente/components/HomeComponent.jsx
+++ marketplace-multivendor-cliente/components/HomeComponent.jsx
@@ -17,9 +17,64 @@ export const HomeComponent = () => {
                 <Col md={4}></Col>
                 <Col md={4}></Col>
                 <Col md={4} style={{margin:"auto"}} className="text-center">
-                    <Button id="btn_comprar_home" variant="outline-info" style={{color:"white", fontSize:"1.5rem", marginRight:"15%"}}>Comienza a comprar</Button>
+                    <Button id="btn_comprar_home" variant="outline-info" style={{color:"white", fontSize:"1.5rem", marginRight:"20%"}}>COMIENZA A COMPRAR</Button>
                 </Col>
             </Row>
+            <Row className="p-4 mt-4 mr-2 ml-2" style={{background:"#dcf7f3"}}>
+                <Col md={12} >
+                    <h4>Productos más vendidos<small>  top+4</small></h4>
+                </Col>
+            </Row>
+            <Row className="pr-4 pl-4 pb-4 mr-2 ml-2" style={{background:"#dcf7f3"}}>
+                <Col md={2} className="p-2">
+                    <Card style={{border:"none"}}>
+                        <Card.Img variant="top" src="/images/yo.jpg" style={{maxHeight: "200px"}}/>
+                        <Card.Body>
+                        <Card.Text>
+                            <p><strong>Chocolate blanco</strong></p>
+                            <p><strong>Precio: </strong>1500</p>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={2} className="p-2">
+                    <Card style={{border:"none"}}>
+                        <Card.Img variant="top" src="/images/yo.jpg" style={{maxHeight: "200px"}}/>
+                        <Card.Body>
+                        <Card.Text>
+                            <p><strong>Chocolate blanco</strong></p>
+                            <p><strong>Precio: </strong>1500</p>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={2} className="p-2">
+                    <Card style={{border:"none"}}>
+                        <Card.Img variant="top" src="/images/yo.jpg" style={{maxHeight: "200px"}}/>
+                        <Card.Body>
+                        <Card.Text>
+                            <p><strong>Chocolate blanco</strong></p>
+                            <p><strong>Precio: </strong>1500</p>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={2} className="p-2">
+                    <Card style={{border:"none"}}>
+                        <Card.Img variant="top" src="/images/yo.jpg" style={{maxHeight: "200px"}}/>
+                        <Card.Body>
+                        <Card.Text>
+                            <p><strong>Chocolate blanco</strong></p>
+                            <p><strong>Precio: </strong>1500</p>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} className="text-center">
+                    <img src="/images/image1.png" width="80%" />
+                </Col>
+            </Row>
+
             <Row>
                 <Col md={12} className="text-center">
                     <InputGroup className="mb-4 mt-4" style={{width:"70%", margin:"auto"}}>
--- marketplace-multivendor-cliente/components/NavbarComponent.jsx
+++ marketplace-multivendor-cliente/components/NavbarComponent.jsx
@@ -16,8 +16,8 @@ export const NavbarComponent = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#features">Productos</Nav.Link>
+                    <Nav.Link href="#pricing">Tiendas</Nav.Link>
                 </Nav>
                 <Nav>
                     <Nav.Link href="#deets">
