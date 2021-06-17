cf11723622ff712d86bbe2e39adc01627b16df03
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2021-06-17 16:13:35 -0600

    Tiendas page

---
+++ marketplace-multivendor-cliente/pages/tiendas.jsx
@@ -0,0 +1,10 @@
+import { Layout } from '../components/layout';
+import { TiendasComprador } from '../components/Tiendas/TiendasComprador';
+
+export default function Tiendas() {
+  return (
+    <Layout>
+        <TiendasComprador />
+    </Layout>
+  )
+}
