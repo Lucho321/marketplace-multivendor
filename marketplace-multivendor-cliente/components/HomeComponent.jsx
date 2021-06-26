ebbdfe68fdb98969eb9acab46bf08086c3d78f0e
Jeffry99 <jeffryzo97@gmail.com>
2021-06-26 05:39:51 -0600

    Update HomeComponent.jsx

--- marketplace-multivendor-cliente/components/HomeComponent.jsx
+++ marketplace-multivendor-cliente/components/HomeComponent.jsx
@@ -3,10 +3,29 @@ import { Button, Col, Row, InputGroup, FormControl, Card } from 'react-bootstrap
 import Image from 'next/image'
 import { ProductCard } from './Productos/ProductCard'
 import { ProductosTienda } from './Productos/ProductosTienda';
+import { getTiendas } from '../services/tiendas.service';
+import { TiendaCard } from './Tiendas/TiendaCard';
+import { useForm } from '../context/hooks/useForm';
 
 export const HomeComponent = () => {
     const [ tipoUsuario, setTipoUsuario ] = useState();
+    const [ tiendas, setTiendas ] = useState([]);
+
     let usuarioLogeado;
+
+    const [ formValues, handleInputChange ] = useForm({
+        buscador: ''
+    });
+    const { buscador } = formValues;
+
+    const getTiendasFilter = (tienda)=>{
+        getTiendas(tienda).then(t=>setTiendas(t));
+    }
+    
+    const handleBuscar = (e)=>{
+        getTiendasFilter(buscador);
+    };
+
     useEffect(() => {
             if (typeof window !== 'undefined') {
                 usuarioLogeado = JSON.parse(localStorage.getItem('_user'));
@@ -16,6 +35,7 @@ export const HomeComponent = () => {
                     
                 }
             }
+            getTiendasFilter("");
     }, [])
 
 
@@ -42,24 +62,15 @@ export const HomeComponent = () => {
                                     </Row>
                                     <Row className="p-4 mt-4 mr-2 ml-2" style={{background:"#eee"}}>
                                         <Col md={12} >
-                                            <h4>Productos más vendidos<small>  top+4</small></h4>
                                         </Col>
                                     </Row>
                                     <Row className="pr-4 pl-4 pb-4 mr-2 ml-2" style={{background:"#eee"}}>
-                                        <Col md={2} className="p-2">
-                                            <ProductCard producto="hola" />
-                                        </Col>
-                                        <Col md={2} className="p-2">
-                                            <ProductCard producto="hola" />
-                                        </Col>
-                                        <Col md={2} className="p-2">
-                                            <ProductCard producto="hola" />
-                                        </Col>
-                                        <Col md={2} className="p-2">
-                                            <ProductCard producto="hola" />
-                                        </Col>
+                                        
                                         <Col md={4} className="text-center">
                                             <img src="/images/image1.png" width="80%" />
+                                        </Col>
+                                        <Col md={8}>
+                                            <Button style={{height:"100px", width:"300px", paddingTop:"30px"}} href="/productos" size="lg" variant="primary">Ver Productos</Button>
                                         </Col>
                                     </Row>
 
@@ -73,68 +84,31 @@ export const HomeComponent = () => {
                                                     placeholder="Escribe aquí el nombre de tu tienda favorita"
                                                     aria-label="Recipient's username"
                                                     aria-describedby="basic-addon2"
+                                                    value={buscador}
+                                                    name="buscador"
+                                                    onChange={ handleInputChange }
                                                 />
                                                 <InputGroup.Append>
-                                                <Button variant="outline-secondary">Buscar</Button>
+                                                    <Button onClick={handleBuscar} variant="outline-secondary">Buscar</Button>
                                                 </InputGroup.Append>
                                             </InputGroup>
                                         </Col>
                                     </Row>
                                     <Row className="p-4 mr-2 ml-2 mb-5" style={{background:"#eee"}}>
-                                        <Col md={3} className="p-2" style={{justifyContent:"center"}}>
-                                            <Card style={{ width: '80%', margin:"auto" }} >
-                                                <Card.Img variant="top" src="/images/yo.jpg" style={{height:"150px"}}/>
-                                                <Card.Body>
-                                                    <Card.Title>Nombre tienda</Card.Title>
-                                                    <Card.Text>
-                                                        Descripcion de la tienda .adasdasdkadklakdlakdlakdakdlkaldk
-                                                    </Card.Text>
-                                                    <Button variant="outline-info" block>Ver tienda</Button>
-                                                </Card.Body>
-                                            </Card>
+                                        <Col md={12} className="p-2" style={{justifyContent:"center"}}>
+                                        {tiendas.map( tienda => (
+                                            <Col  md={10} className="mb-3">
+                                                <TiendaCard key={tienda.id_tienda} tienda={tienda}/>
+                                            </Col>
+                                        ))}
                                         </Col>
-                                        <Col md={3} className="p-2" >
-                                            <Card style={{ width: '80%', margin:"auto" }} >
-                                                <Card.Img variant="top" src="/images/yo.jpg" style={{height:"150px"}}/>
-                                                <Card.Body>
-                                                    <Card.Title>Nombre tienda</Card.Title>
-                                                    <Card.Text>
-                                                        Descripcion de la tienda .adasdasdkadklakdlakdlakdakdlkaldk
-                                                    </Card.Text>
-                                                    <Button variant="outline-info" block>Ver tienda</Button>
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
-                                        <Col md={3} className="p-2" style={{justifyContent:"center"}}>
-                                            <Card style={{ width: '80%', margin:"auto" }} >
-                                                <Card.Img variant="top" src="/images/yo.jpg" style={{height:"150px"}} />
-                                                <Card.Body>
-                                                    <Card.Title>Nombre tienda</Card.Title>
-                                                    <Card.Text>
-                                                        Descripcion de la tienda .adasdasdkadklakdlakdlakdakdlkaldk
-                                                    </Card.Text>
-                                                    <Button variant="outline-info" block>Ver tienda</Button>
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
-                                        <Col md={3} className="p-2" style={{justifyContent:"center"}}>
-                                            <Card style={{ width: '80%', margin:"auto" }} >
-                                                <Card.Img variant="top" src="/images/yo.jpg" style={{height:"150px"}} />
-                                                <Card.Body>
-                                                    <Card.Title>Nombre tienda</Card.Title>
-                                                    <Card.Text>
-                                                        Descripcion de la tienda .adasdasdkadklakdlakdlakdakdlkaldk
-                                                    </Card.Text>
-                                                    <Button variant="outline-info" block>Ver tienda</Button>
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
+                                        
                                     </Row>
                                 </>
             )
         }
         {
-        tipoUsuario===0 && <ProductosTienda />
+            tipoUsuario===0 && <ProductosTienda />
         }
         </>
         
